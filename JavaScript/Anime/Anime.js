4ea77d0f508ee51fe7121e890c56c4e960106e9f
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-02-19 14:19:29 -0500

    Completed search fuction

--- JavaScript/Anime/Anime.js
+++ JavaScript/Anime/Anime.js
@@ -1,9 +1,24 @@
+const Genres={
+    "Action": 1,
+    "Adventure:":2,
+    "Cars:":3,
+    "Comedy":4,
+    "Dementia":5,
+    "Demons":6,
+    "Mystery":7,
+    "Drama":8,
+    "Ecchi":9,
+    "Fantasy":10,
+    "Game":11
+
+}
 const getMangas=function(){
     $.get(
         "https://api.jikan.moe/v3/manga/1",
         function(data, testState,jqXHR ){
             $("#Manga1").text(`${data.title}`)
-            $('#icon1').attr('src',`${data.image_url}`)
+            $('#icon1').attr('src',`${data.image_url}`);
+            $('#Manga1Desc').text(`${data.synopsis}`);
             console.log(data);
             console.log(testState);
             console.log(jqXHR);
@@ -14,6 +29,7 @@ const getMangas=function(){
         function(data, testState,jqXHR ){
             $("#Manga2").text(`${data.title}`)
             $('#icon2').attr('src',`${data.image_url}`)
+            $('#Manga2Desc').text(`${data.synopsis}`);
             console.log(data);
             console.log(testState);
             console.log(jqXHR);
@@ -24,6 +40,7 @@ const getMangas=function(){
         function(data, testState,jqXHR ){
             $("#Manga3").text(`${data.title}`)
             $('#icon3').attr('src',`${data.image_url}`)
+            $('#Manga3Desc').text(`${data.synopsis}`);
             console.log(data);
             console.log(testState);
             console.log(jqXHR);
@@ -34,10 +51,34 @@ const getMangas=function(){
         function(data, testState,jqXHR ){
             $("#Manga4").text(`${data.title}`)
             $('#icon4').attr('src',`${data.image_url}`)
+            $('#Manga4Desc').text(`${data.synopsis}`);
             console.log(data);
             console.log(testState);
             console.log(jqXHR);
         }
     );
+    $.get(
+        "https://api.jikan.moe/v3/manga/7",
+        function(data, testState,jqXHR ){
+            $("#Manga7").text(`${data.title}`)
+            $('#icon7').attr('src',`${data.image_url}`)
+            $('#Manga7Desc').text(`${data.synopsis}`);
+            console.log(data);
+            console.log(testState);
+            console.log(jqXHR);
+        }
+    );
+}
+const SearchManga=function(){
+const Genre=$('#Genre').val;
+
+if(Genre===`${Genres.key}`){
+    $.get(`https://api.jikan.moe/v3/search/manga?genre=${Genres.key.val}genre_exclude=0`),
+    function(data,testState,jqXHR){
+        console.log(data);
+            console.log(testState);
+            console.log(jqXHR);
+    }
+}
 }
 getMangas();
