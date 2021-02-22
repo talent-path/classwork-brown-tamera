f44e2f30d6b73c9a8c97c7d6e80e3d34c1ef5738
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-02-21 23:04:48 -0500

    Made Manga display into cards

--- JavaScript/Anime/Anime.js
+++ JavaScript/Anime/Anime.js
@@ -12,50 +12,63 @@ const getMangas=function(){
             let bodyTag= allbodyTags[0];
             let animeDivTag=document.createElement('div');
             bodyTag.appendChild(animeDivTag);
+            let card= document.createElement('div');
+            card.className="card";
+            animeDivTag.appendChild(card);
 
             for(let i=0;i<data.results.length;i++){
-                let animeHeader=document.createElement('h3');
-                animeHeader.id="Manga "+ data.results[i] + "header";
-                $('h3').text(`${data.results[i].title}`);
-                animeDivTag.appendChild(animeHeader);
+               
+                let animeHeader=document.createElement('h4');
+                animeHeader.id="Manga "+ i + " header";
+                animeHeader.className="card-title";
+                animeHeader.innerHTML=`${data.results[i].title}`;
+                card.appendChild(animeHeader);
                 let animeImg=document.createElement('img');
-                animeImg.id="Ma"
-                $('img').attr('src',`${data.results[i].image_url}`);
-                animeDivTag.appendChild(animeImg);
+                animeImg.id="Manga " + i + " image";
+                animeImg.className="card-img";
+                animeImg.setAttribute("src",`${data.results[i].image_url}`);
+
+                animeImg.style.width="20%";
+                card.appendChild(animeImg);
                 let animeDesc=document.createElement('article');
-                $('animeDesc').text(`${data.results[i].synopsis}`);
-                animeDivTag.appendChild(animeDesc);
+                animeDesc.id="Manga " + i + " description";
+                animeDesc.className="card-text";
+                animeDesc.innerHTML=`${data.results[i].synopsis}`;
+                card.appendChild(animeDesc);
 
             }
-
+            
            
         }
     );
-    
+   
     }
 
 const SearchManga=function(){
 
 const Genre=document.getElementById('Genre').value;
-console.log(Genre);
 
-    
-    $.get(`https://api.jikan.moe/v3/search/manga?genre=${Genre}`,
+
+    $.get(`https://api.jikan.moe/v3/search/manga?genre=${Genre}limit=10`,
     function(data,testState,jqXHR){
-        
-        for(let i=0;i<data.results.length;i++){
 
-        $('h3').text(`${data.results[i].title}`)
-        $('img').attr('src',`${data.results[i].image_url}`);
-        $('article').text(`${data.results[i].synopsis}`);
-        }
-            console.log(data);
+           console.log(data);
             console.log(testState);
             console.log(jqXHR);
     
+        for(let i=0;i<data.results.length;i++){
+            let animeHeader=document.getElementById('Manga ' + i + ' header');
+    
+        animeHeader.innerHTML=`${data.results[i].title}`;
+        let animeImg=document.getElementById('Manga ' + i + ' image');
+        animeImg.setAttribute("src",`${data.results[i].image_url}`);
+        let animeDesc=document.getElementById('Manga ' + i + ' description');
+        animeDesc.innerHTML=`${data.results[i].synopsis}`;
+        }
+            
     }
     )
-
+console.log(Genre);
     
 }
 getMangas();
